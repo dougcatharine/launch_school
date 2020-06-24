404b3db000f337aefdf55e5650e38d9518f4a4cf
Doug Catharine <dougcatharine@gmail.com>
2020-06-24 13:03:05 -0600

    updating loops and iterators

--- book_intro_to_ruby/loops_and_iterators/ex_1.rb
+++ book_intro_to_ruby/loops_and_iterators/ex_1.rb
@@ -1,1 +1,15 @@
-ex_1.rb
+# ex_1.rb
+# Doug CAtharine
+# 20200624
+
+=begin
+What does the each method in the following program return after it is finished 
+executing?
+=end
+
+
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+# the each method iterates through each element in the x array
