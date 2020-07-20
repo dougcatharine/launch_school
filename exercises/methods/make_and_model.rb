5704d3a633da2751d698c0aa2a50522d6a86653d
Doug Catharine <dougcatharine@gmail.com>
2020-07-19 22:21:54 -0600

    Adding methods

---
+++ exercises/methods/make_and_model.rb
@@ -0,0 +1,18 @@
+# make_and_model.rb
+# Doug Catharine
+# 20200719
+
+=begin
+Using the following code, write a method called car that takes two arguments and prints a string containing the values of both arguments.
+
+car('Toyota', 'Corolla')
+Expected output:
+
+Toyota Corolla
+=end
+
+def car(make, model)
+  puts make + ' ' + model
+end
+
+car('Toyota', 'Corolla')
