2248f9ebb52a89cd75e8a00004e84993cd5b621e
Doug Catharine <dougcatharine@gmail.com>
2022-10-18 14:59:49 -0600

    updating to include methods chapter

---
+++ book_intro_to_ruby/Methods/Ex_4.rb
@@ -0,0 +1,22 @@
+#Ex_4.rb
+#Doug Catharine
+#20221018
+
+
+
+
+=begin
+What will the following code print to the screen?
+
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+no print value
+
+=end
---
+++ book_intro_to_ruby/Methods/ex_2.rb
@@ -0,0 +1,20 @@
+#ex_2.rb
+#Doug Catharine
+#20221018
+
+
+=begin
+What do the following expressions evaluate to?
+
+
+1. x = 2
+2
+2. puts x = 2
+nil
+3. p name = "Joe"
+"joe"
+4. four = "four"
+"four"
+5. print something = "nothing"
+nil
+=end
