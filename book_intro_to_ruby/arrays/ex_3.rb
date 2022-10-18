9e4a7232e14a3579dea483298234f325083f5d76
Doug Catharine <dougcatharine@users.noreply.github.com>
2022-10-18 11:56:18 -0600

    Delete book_intro_to_ruby/arrays directory

--- book_intro_to_ruby/arrays/ex_3.rb
+++
@@ -1,8 +0,0 @@
-# ex_3.rb
-# Doug Catharine
-# 20200630
-
-# How do you return the word "example" from the following array?
-arr = [["test", "hello", "world"],["example", "mem"]]
-
-puts arr.last.first
